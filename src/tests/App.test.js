12051842d426db3867df23f06a1316d725682800
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-01-28 15:38:18 -0300

    add req 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -7,19 +7,19 @@ import App from '../App';
 describe('Teste do componente App', () => {
   it('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
     renderWithRouter(<App />);
+    const favPokemon = 'Favorite Pokémons';
 
     const linkHome = screen.getByRole('link', { name: 'Home' });
     const linkAbout = screen.getByRole('link', { name: 'About' });
-    const linkFavorite = screen.getByRole('link', {
-      name: 'Favorite Pokémons',
+    const linkFavorite = screen.getByRole('link', { name: favPokemon,
     });
 
     expect(linkHome).toHaveTextContent('Home');
     expect(linkAbout).toHaveTextContent('About');
-    expect(linkFavorite).toHaveTextContent('Favorite Pokémons');
+    expect(linkFavorite).toHaveTextContent(favPokemon);
   });
 
-  it('Teste se a aplicação é redirecionada para a página inicial', () => {
+  it('Teste se a aplicação é redirecionada para a página Home', () => {
     const { history } = renderWithRouter(<App />);
 
     history.push('/about');
@@ -37,5 +37,44 @@ describe('Teste do componente App', () => {
       name: 'Encountered pokémons',
     });
     expect(textHome).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página About', () => {
+    renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    userEvent.click(linkAbout);
+
+    const textAbout = screen.getByRole('heading', {
+      level: 2,
+      name: 'About Pokédex',
+    });
+    expect(textAbout).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Favorite Pokémons', () => {
+    renderWithRouter(<App />);
+
+    const linkFavorite = screen.getByRole('link', {
+      name: 'Favorite Pokémons',
+    });
+    userEvent.click(linkFavorite);
+
+    const textFavorite = screen.getByRole('heading', {
+      level: 2,
+      name: 'Favorite pokémons',
+    });
+    expect(textFavorite).toBeInTheDocument();
+  });
+
+  it('Teste se a aplicação é redirecionada para a página Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+    console.log(history);
+
+    history.push('/desconhecida');
+
+    const textDesconhecida = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(textDesconhecida).toBeInTheDocument();
   });
 });
